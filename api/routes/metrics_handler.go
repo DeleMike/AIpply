56d7f8cf21ddef063dab99b46e7c8a5033f22211
DeleMike <akindelemichael65@gmail.com>
2025-10-23 09:52:47 +0100

    fix cors issue and linting

--- api/routes/metrics_handler.go
+++ api/routes/metrics_handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetMetrics send merics about the current CVs and cover letters generated
 func GetMetrics(c *gin.Context) {
 	response := metrics.GetMetrics()
 	c.JSON(http.StatusOK, response)
